3fddf27b10c87a3eee68825af3daedac56d8c52e
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2020-01-25 15:29:27 -0300

    Inplementando websockets

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 /* Importando as configurações do servidor */
 var app = require('./config/server');
 
-/* parametrizar a porta de escuta */
-app.listen(5500,function(){
+/* Parametrizar a porta de escuta */
+var server = app.listen(5500,function(){
     console.log('Servidor Online')
+});
+
+
+/* Integrando o WebSockets na aplicacao  */
+var io = require('socket.io').listen(server);
+
+/* Criar a conexão por WebSocket */
+io.on('connect',function(socket){
+    console.log('Usuário Conectou');
+
+    socket.on('disconnect',function(){
+        console.log('Usuário desconectou');
+    });
+
+    
+
 });
